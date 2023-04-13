fbd2926589efb8227be65cd39f3b1bb3a13062a5
Ye.Lin <Ye.Lin@cnexlabs.com>
2023-04-13 10:18:52 +0800

    move lsp config to other space

--- init.lua
+++ init.lua
@@ -3,9 +3,12 @@ local version = vim.version
 
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~\\AppData\\Local\\nvim\\plugged')
+-- below are some vim plugins for demonstration purpose.
+-- add the plugin you want to use here.
 Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
 Plug 'neovim/nvim-lspconfig'
 -- what about Plug?...
 
 vim.call('plug#end')
+require'pylsp_cfg'
